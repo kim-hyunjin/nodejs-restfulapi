8a1b322237f3915b1b02f6e6cd0f72b51e3118cb
kim-hyunjin <hyunjin1612@gmail.com>
2020-10-25 16:21:12 +0900

    db 연결 및 스키마 정의

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Book = require('./models/book');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -13,3 +15,11 @@ var router = require('./routes')(app);
 var server = app.listen(port, function() {
   console.log("Express server has started on port : " + port);
 });
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function() {
+  console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://localhost/mongodb_tutorial');
